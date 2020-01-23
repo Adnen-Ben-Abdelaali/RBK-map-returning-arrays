70ce0f6eddeee15bdcca4609c1cffa455bd2aedd
AdnenBenAbdelaali <benabdelaaliadnen@gmail.com>
2020-01-23 20:53:53 +0100

    fonction reverse(words) implementée de 0

--- script.js
+++ script.js
@@ -82,3 +82,108 @@ function max(array) {
   });
   return maximum;
 }
+/*****************************/
+function maximums(arrayOfArrays) {
+  
+  return map(arrayOfArrays, function(elementOfArrays, indexOfArrays) {
+    let maximum = -Infinity;
+    each(elementOfArrays, function(element, index) {
+      if(maximum < element) {
+        maximum = element;
+      }
+    });
+    return maximum; 
+  });
+}
+/**************************************************************/
+/*
+1.Complete the function exponentials that accepts an array
+ of numbers as a parameter, and raises each number n to 
+ the nth power, e.g:
+
+  function exponentials(numbers) { 
+       // YOUR CODE HERE 
+ } 
+ exponentials([1, 2, 3, 4]);
+  // => [1, 2*2, 3*3*3, 4*4*4*4] => [1, 4, 27, 256] 
+*/
+function exponentials(numbers) {
+  
+  return map(numbers, function(element, index){
+    let exp = 1;
+    for(let i = 0; i < index + 1; i++) {
+      exp *= element;
+    }
+    return exp;
+  });
+}
+/**************************************************************/
+/*
+2.First, write a function reverse that accepts a string
+ as a parameter and returns a reversed version of that string 
+ (you'll want to use a for loop for this). Then, use reverse to
+  write a function called reverseWords that accepts a string as 
+  an argument, and returns a string with all of its words reversed. 
+  
+ reverseWords('hello world'); // => 'olleh dlrow'
+
+Note that reverseWords should reverse each word 
+individually, not the entire string.
+*/
+/* function reverseOneWord(word) reverses a word */
+function reverseOneWord(word) {
+  let reversedWord = '';
+  let i = word.length - 1;
+  while(i >= 0) {
+    reversedWord += word[i];
+    i--;
+  }
+  return reversedWord;
+}
+/* function identifyWords(words) identifies the number of words in a
+string and returns a table containing those words */
+function identifyWords(words) {
+  let arrayOfWords = new Array();
+  let spaceIndexes = new Array();
+  
+  for(let i = 0; i < words.length; i++) {
+    if(words[i] === " ") {
+      spaceIndexes.push(i);
+    }
+  }
+  let stoppingIndex = -1;
+  each(spaceIndexes, function(element, index) {
+    let word = "";
+    for(let i = stoppingIndex + 1; i < element; i++) {
+      word += words[i];
+    }
+    stoppingIndex = element;
+    if ( (word != " ") && (word != "") ) {
+      arrayOfWords.push(word);
+    }
+  });
+  /* Getting the last word of "words" */
+  let lastWord = "";
+  for(let i = spaceIndexes[spaceIndexes.length - 1]; i < words.length; i++) {
+    lastWord += words[i];
+  }
+  /* checking that the last word is not composed of empty spaces */
+if ( (lastWord != " ") && (lastWord != "") ) {
+  arrayOfWords.push(lastWord);
+}
+  return arrayOfWords;
+}
+/* reversing the words and returning them as a string*/ 
+function reverse(words) {
+  let reversedString = '';
+  let tableOfWords = new Array();
+
+  tableOfWords = identifyWords(words);
+  each(tableOfWords, function(element, index) {
+    if(element != tableOfWords[tableOfWords.length - 1]) {
+      reversedString += reverseOneWord(element) + " ";
+    }
+  });
+  reversedString += reverseOneWord(tableOfWords[tableOfWords.length - 1]);
+  return reversedString;
+}
